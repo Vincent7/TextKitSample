b9c66f7f70581dd0a23e4071d522e178a472f72d
Vincent <and7@live.cn>
2017-04-20 11:50:00 +0800

    Initial Commit

---
+++ TextKitSample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TextKitSample
+//
+//  Created by Vincent on 2017/4/20.
+//  Copyright © 2017年 Vincent. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
